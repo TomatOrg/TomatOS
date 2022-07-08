aaf8d8df7be4a7bfbb1fae00e538c38841662f54
Itay Almog <itay2828@gmail.com>
2022-07-08 19:48:20 +0300

    updated tinydotnet

--- Pentagon/Pentagon/Kernel.cs
+++ Pentagon/Pentagon/Kernel.cs
@@ -1,35 +1,13 @@
-using System;
-using System.Collections.Generic;
-using System.Runtime.InteropServices;
-using System.Runtime.CompilerServices;
-using System.Threading;
-using System.Threading.Tasks;
-
 namespace Pentagon;
 
 public class Kernel
 {
 
-    interface A
-    {
-        
-    }
-
-    interface B : A
-    {
-        
-    }
-
-    interface C : B
-    {
-        
-    }
-    
     public static int Main()
     {
-        var framebufferOwner = MemoryServices.MapPages(0x00000000FD000000, 1);
-        framebufferOwner.Dispose();
-        return Unsafe.SizeOf<int>();
+        using var framebufferOwner = MemoryServices.MapPages(0xFD000000, 10);
+        framebufferOwner.Memory.Span.Fill(0xFF);
+        return 0;
     }
     
 }
