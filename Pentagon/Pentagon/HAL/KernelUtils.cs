7009ee70a077da87f02e7172194d02a62c35c700
Itay Almog <itay2828@gmail.com>
2022-07-11 23:12:54 +0300

    actual bugfix

--- Pentagon/Pentagon/HAL/KernelUtils.cs
+++ Pentagon/Pentagon/HAL/KernelUtils.cs
@@ -6,12 +6,12 @@ public static class KernelUtils
     
     public static ulong AlignDown(ulong value, ulong alignment)
     {
-        return (value + (alignment - 1)) & ~(alignment - 1);
+        return value - (value & (alignment - 1));
     }
 
     public static ulong AlignUp(ulong value, ulong alignment)
     {
-        return value - (value & (alignment - 1));
+        return (value + (alignment - 1)) & ~(alignment - 1);
     }
 
 }
