bb067962b3d9c3e4a4d930fea5d2f7d4c848b72c
Itay Almog <itay2828@gmail.com>
2022-07-28 23:39:28 +0100

    fixed obj to interface null cast

--- Pentagon/Pentagon/Kernel.cs
+++ Pentagon/Pentagon/Kernel.cs
@@ -1,4 +1,5 @@
 using System;
+using Pentagon.Drivers;
 using Pentagon.Drivers.Virtio;
 using Pentagon.DriverServices;
 using Pentagon.DriverServices.Acpi;
@@ -10,14 +11,11 @@ public class Kernel
     public static int Main()
     {
         // setup the basic subsystems
-        // var acpi = new Acpi();
-        // Pci.Scan(acpi);
-        //
-        // // register built-in drivers
-        // VirtioDevice.Register();
-
-        var obj = Activator.CreateInstance(typeof(MissingMemberException), "className", "memberName");
-        Log.LogString(obj.ToString());
+        var acpi = new Acpi();
+        Pci.Scan(acpi);
+        
+        // register built-in drivers
+        VirtioDevice.Register();
 
         return 0;
     }
