4452d5e9d0371eb670a9d976b9d71b1b70d8d381
StaticSaga <61866965+StaticSaga@users.noreply.github.com>
2022-10-05 11:41:14 +0200

    fix PM timer on real hardware

--- kernel/acpi/acpi.h
+++ kernel/acpi/acpi.h
@@ -2,6 +2,23 @@
 
 #include "util/except.h"
 
+typedef struct acpi_common_header {
+    uint32_t signature;
+    uint32_t length;
+} PACKED acpi_common_header_t;
+
+typedef struct acpi_descriptor_header {
+    uint32_t signature;
+    uint32_t length;
+    uint8_t revision;
+    uint8_t checksum;
+    uint8_t oem_id[6];
+    uint64_t oem_table_id;
+    uint32_t oem_revision;
+    uint32_t creator_id;
+    uint32_t creator_revision;
+} PACKED acpi_descriptor_header_t;
+
 /**
  * Pointer to the RSDT, for use in C#
  */
