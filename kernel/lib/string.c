e81ef693cd308418ae0e26a1aa93d21977c80db1
Itay Almog <itay2828@gmail.com>
2025-01-04 00:57:35 +0200

    better memmove

--- kernel/lib/string.c
+++ kernel/lib/string.c
@@ -7,9 +7,8 @@
 #include "defs.h"
 
 void* memset(void* s, int c, size_t n) {
-    // fast path for zero length
-    if (n == 0) return s;
-
+    // NOTE: we assume that fast short rep stosb is supported, meaning that
+    //       0-128 length strings should be fast
     void* d = s;
     asm volatile (
         "rep stosb"
@@ -20,38 +19,64 @@ void* memset(void* s, int c, size_t n) {
     return d;
 }
 
-void* memcpy(void* restrict dest, const void* restrict src, size_t n) {
-    // fast path for zero length
-    if (n == 0) return dest;
-
-    void* d = dest;
+__attribute__((always_inline))
+static inline void __rep_movsb(void* dest, const void* src, size_t n) {
     asm volatile (
         "rep movsb"
         : "+D"(dest), "+S"(src), "+c"(n)
         :
         : "memory"
     );
-    return d;
+}
+
+void* memcpy(void* restrict dest, const void* restrict src, size_t n) {
+    // fast path for zero length
+    // TODO: patch away if we have fast zero-length rep movsb
+    if (UNLIKELY(n == 0))
+        return dest;
+
+    __rep_movsb(dest, src, n);
+    return dest;
 }
 
 void* memmove(void* dest, const void* src, size_t n) {
     // fast path for zero length or the same exact buffer
-    if (n == 0 || (dest == src)) {
+    if (UNLIKELY(n == 0) || (dest == src)) {
         return dest;
     }
 
-    if (dest < src) {
-        return memcpy(dest, src, n);
+    void* d = dest;
+
+    if (src < dest && dest < src + n) {
+        // calculate the backwards copy size
+        size_t tail_size = dest - src;
+
+        // we are going to copy backwards at a chunk size of the non-overlapping
+        // area, the tail size is always smaller than n at the start, so we can
+        // use a do-while, and we are starting from that area
+        dest += n - tail_size;
+        src += n - tail_size;
+        do {
+            // perform the forward copy
+            __rep_movsb(dest, src, tail_size);
+
+            // and now move the pointers and size backwards
+            dest -= tail_size;
+            src -= tail_size;
+            n -= tail_size;
+        } while (n > tail_size);
+
+        // if we are left with non-zero size then we
+        // just need to perform this one last copy
+        if (n != 0) {
+            __rep_movsb(dest, src, n);
+        }
+    } else {
+        // not overlapping, use a normal copy
+        __rep_movsb(dest, src, n);
     }
 
-    // perform a normal slow backwards copy
-    char* d = dest;
-    const char* s = src;
-    while (n--) {
-        d[n] = s[n];
-    }
-
-    return dest;
+    return d;
 }
 
 int memcmp(const void* vl, const void* vr, size_t n) {
@@ -75,12 +100,13 @@ int strcmp(const char* l, const char* r) {
 
 void string_verify_features(void) {
     uint32_t eax, ebx, ecx, edx;
-    __cpuid(7, 0, eax, ebx, ecx, edx);
 
+    __cpuid(7, 0, eax, ebx, ecx, edx);
     if ((ebx & BIT9) == 0) LOG_WARN("string: Missing enhanced REP MOVSB/STOSB");
     if ((edx & BIT4) == 0) LOG_WARN("string: Missing fast short REP MOVSB");
 
-    // __cpuid(7, 1, eax, ebx, ecx, edx);
+    __cpuid(7, 1, eax, ebx, ecx, edx);
     // if ((eax & BIT10) == 0) LOG_WARN("string: Missing zero-length REP MOVSB");
+    if ((eax & BIT11) == 0) LOG_WARN("string: Missing fast short REP STOSB");
     // if ((eax & BIT12) == 0) LOG_WARN("string: Missing fast short REP CMPSB/CSASB");
 }
